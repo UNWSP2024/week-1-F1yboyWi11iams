0caacab1ad735c052da1203ef42d06242684b923
F1yboyWi11iams <sjwilliams3@students.unwsp.edu>
2024-09-02 11:01:57 -0500

    Update program_2.py
    Williams Edit

--- program_2.py
+++ program_2.py
@@ -4,3 +4,7 @@ def proverbs_22_29():
     
 
 proverbs_22_29()
+	print(""Do you see a man skillful in his work?"")
+	print("He will stand up before kings;")
+	print("He will not stand before obscure men.")
+ exit()
